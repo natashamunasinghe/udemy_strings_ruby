00006a605683bf07c9da93283a68b16e992c23e0
Your Name <you@example.com>
2018-10-10 20:50:54 +1200

    add arrays code

---
+++ Udemy_arrays.rb
@@ -0,0 +1,30 @@
+#use of Ruby arrays
+a=[1, 2, 3, 3, "Hello", ["x", "y", "z"]]
+
+#array methods
+# puts a.reverse
+# puts a.length
+# puts a.pop(3)
+# puts a.push("Hi")
+# puts a.unshift(0)
+# puts a.uniq
+
+
+#use of iterators
+
+#Each loop
+# a.each { | i | print i }
+# a.each { | i | i.capitalize }
+# puts i 
+
+a.each do |int|
+puts "Hello"
+end
+
+# #For Loop
+# for 
+#     number in a 
+#     puts "Ho"
+# end 
+
+
